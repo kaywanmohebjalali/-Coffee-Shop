3413da7d6e024b8ce9d5759dc25303ec7aa3fccc
kaywan mohebjalali <mohebjalali5282@gmail.com>
2024-04-16 18:20:05 +0330

    create new header

--- components/modules/Header/Header.tsx
+++ components/modules/Header/Header.tsx
@@ -1,41 +1,88 @@
-import React from 'react'
-import styled from './header.module.scss'
+import React, { useEffect, useRef, useState } from 'react'
+import styled from './header.module.css'
 import Link from 'next/link'
 import { FaAngleDown } from "react-icons/fa6";
+let [heightParent,child]=['',''] as any
 const Header = () => {
-  return (
-    <div className="container my-8 ">
+ 
+  const parentRef = useRef(null)
+  const childRef = useRef(null)
 
-      <nav className=''>
-      <h1 className={styled.text}>COFFEE</h1>
-        <ul className={styled.list}>
+  function close(){
+    heightParent = ((parentRef?.current as any)?.style)
+    child = childRef?.current as any;
+    heightParent.height = '25px';
+    child?.classList?.remove(styled.pagesChildrenActive);
+    child.childNodes[0].style.display = 'none'
+    child.childNodes[1].style.display = 'none'
+
+  }
+
+  function dropDownHandle() {
+     heightParent = ((parentRef?.current as any)?.style)
+     child = childRef?.current as any;
+
+    console.log(child?.childNodes[0].style.display);
+    let w = window.innerWidth
+     
+    if (child.childNodes[0].style.display=='none' || child.childNodes[0].style.display=='') {
+  
+      heightParent.height =`${w<1000?'80px':'0'}`;
+      child?.classList?.add(styled.pagesChildrenActive);
+      child.childNodes[0].style.display = 'block'
+      child.childNodes[1].style.display = 'block'
+
+
+    } else {
+  
+   close()
+
+    }
+
+  }
+
+  useEffect(()=>{
+     (window as any)?.addEventListener('resize',()=>{
+      close()
+     })
+  },[])
+
+
+  return (
+    <div className="my-2 px-[1rem]">
+
+      <nav className={`${styled.navStyle}  `}>
+        <h1 className={styled.text}>COFFEE</h1>
+        <ul className={`${styled.list} px-[3.5rem] `} >
           <li><Link href='/'>Home</Link></li>
           <li><Link href='/about'>About</Link></li>
           <li><Link href='/service'>Service</Link></li>
           <li><Link href='/menu'>Menu</Link></li>
-          <li className=''><Link href='/contact' >Contact</Link></li>
-          
-            <li className={`${styled.pages}`}>
-              <div className=" flex items-center gap-1">
 
-              <p className={`${styled.textPages} mt-[-5px]`}>
+
+          <li ref={parentRef} className={`${styled.pages}`} onClick={dropDownHandle}>
+            <div className=" flex items-center gap-1">
+
+              <p className={`${styled.textPages} mt-[-5px] sm:mt-[0]`}>
                 Pages
-                
-
-                </p>
-            <FaAngleDown size='0.9rem'/>
-              </div>
 
 
-              <div className={styled.pagesChildren}>
+              </p>
+              <FaAngleDown size='0.9rem' />
+            </div>
 
-              <Link className={styled.reservation} href='/reservation'>Reservation</Link>
+
+            <div ref={childRef} className={`${styled.pagesChildren}`}>
+
+              <Link className={`${styled.reservation}`} href='/reservation'>Reservation</Link>
               <Link className={styled.testimonial} href='/testimonial'>Testimonial</Link>
-              </div>
-                
+            </div> 
 
-            </li>
-          
+
+
+          </li>
+          <li className=''><Link href='/contact' >Contact</Link></li>
+
         </ul>
       </nav>
 
